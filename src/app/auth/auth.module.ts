40b7ed70782e0c5aded51eb8b19872132231dff2
felipel7 <felipelseven@gmail.com>
2024-10-08 10:15:01 -0300

    feat(AdminLogin): criar layout da pagina com o form

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AdminLoginComponent } from './login/admin-login/admin-login.component';
 
-
 @NgModule({
-  declarations: [
-    AdminLoginComponent
-  ],
+  declarations: [AdminLoginComponent],
   imports: [
+    AuthRoutingModule,
     CommonModule,
-    AuthRoutingModule
-  ]
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/auth/login/admin-login/admin-login.component.ts
+++ src/app/auth/login/admin-login/admin-login.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-login',
   templateUrl: './admin-login.component.html',
-  styleUrls: ['./admin-login.component.scss']
+  styleUrls: ['./admin-login.component.scss'],
 })
 export class AdminLoginComponent {
+  authForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
+  constructor(private router: Router) {}
+
+  onSubmit($event: SubmitEvent) {
+    $event.preventDefault();
+
+    console.log({ form: this.authForm.value });
+    this.router.navigate(['/']);
+  }
 }
