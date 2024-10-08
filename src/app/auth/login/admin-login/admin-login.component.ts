f0b230154291d55a5ee6edd6981dc727ef0ad442
felipel7 <felipelseven@gmail.com>
2024-10-08 12:48:08 -0300

    feat(AdminLogin): adicionar validacao dos campos do form

--- src/app/auth/login/admin-login/admin-login.component.ts
+++ src/app/auth/login/admin-login/admin-login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,8 +9,8 @@ import { Router } from '@angular/router';
 })
 export class AdminLoginComponent {
   authForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
   });
 
   constructor(private router: Router) {}
