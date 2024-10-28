1f3eb5022fad894f6233323914d6625854a81d33
Anna <acesbrama@gmail.com>
2024-10-28 18:57:31 -0300

    fix: alteração de commit para organização de branch

--- src/app/pages/admin-screen/admin-screen.component.ts
+++ src/app/pages/admin-screen/admin-screen.component.ts
@@ -29,9 +29,4 @@ export class AdminScreenComponent {
   intoToClassRegistration() {
     this.router.navigate(['/class-registration']);
   }
-
-//Direcionamento do botão "Sair" para a página de login do administrador.
-  logout(){
-    this.router.navigate(['/admin-login']);
-  }
 }
