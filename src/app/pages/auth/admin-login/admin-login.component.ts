8b8ca78e867845431414793ea8e43efa8554aee3
Marcos Vinícius <santsmarcos81@gmail.com>
2024-10-30 19:06:03 -0300

    Ajuste na estrutura de pastas (#5)
    * Telas de escolha de perfile login para professores e responsaveis, rotas, validacoes de campo e responsividade
    
    * Paginas de autenticacao dentro de auth
    
    * Unificacao css tela login-guardina/login-teacher
    
    * Unificacao css tela login-guardian/login-teacher
    
    * Implementacao de estilo no app.component
    
    * Configurada rota para tela administrador
    
    * Corrigindo estrutura de pastas
    
    * Estilo de botao desabilitado e ajuste de logo na tela login professor e pais
    
    * Inserido mensagem para validacao de input required

--- src/app/pages/auth/admin-login/admin-login.component.ts
+++ src/app/pages/auth/admin-login/admin-login.component.ts
@@ -7,7 +7,7 @@ import {
   AuthService,
   LoginCredentials,
   LoginResponse,
-} from '../../auth.service';
+} from '../auth.service';
 
 @Component({
   selector: 'app-admin-login',
@@ -38,11 +38,18 @@ export class AdminLoginComponent {
     });
   }
 
+  isInvalidRequired(controlName: string): boolean {
+    const control = this.authForm.get(controlName);
+    return control?.hasError('required') && control.touched ? true : false;
+  }
+
   isInvalidEmailFormat(): boolean {
+    const control = this.authForm.get('email');
     return (
-      !this.authForm.controls.email.hasError('required') &&
-      this.authForm.controls.email.hasError('email') &&
-      this.authForm.controls.email.touched
+      control != null &&
+      !control.hasError('required') &&
+      (control.hasError('email') ?? false) &&
+      control.touched
     );
   }
 
--- src/app/pages/auth/auth-routing.module.ts
+++ src/app/pages/auth/auth-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { SelectProfileComponent } from './select-profile/select-profile.component';
 import { LoginTeacherComponent } from './login-teacher/login-teacher.component';
 import { LoginGuardianComponent } from './login-guardian/login-guardian.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 const routes: Routes = [
   { path: '', component: SelectProfileComponent },
   { path: 'login/professor', component: LoginTeacherComponent },
   { path: 'login/responsavel', component: LoginGuardianComponent },
+  { path: 'login/administrador', component: AdminLoginComponent }
 ];
 
 @NgModule({
