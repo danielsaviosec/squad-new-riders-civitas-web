7f4a685781b230b4cad94c98db3f16e786c22e73
Vinícius <santsmarcos81@gmail.com>
2024-10-30 17:59:54 -0300

    Inserido mensagem para validacao de input required

--- src/app/pages/auth/admin-login/admin-login.component.ts
+++ src/app/pages/auth/admin-login/admin-login.component.ts
@@ -38,11 +38,18 @@ export class AdminLoginComponent {
     });
   }
 
+  isInvalidRequired(controlName: string): boolean {
+    const control = this.authForm.get(controlName);
+    return control?.hasError('required') && control.touched ? true : false;
+  }
+
   isInvalidEmailFormat(): boolean {
+    const control = this.authForm.get('email');
     return (
-      !this.authForm.controls.email.hasError('required') &&
-      this.authForm.controls.email.hasError('email') &&
-      this.authForm.controls.email.touched
+      control != null &&
+      !control.hasError('required') &&
+      (control.hasError('email') ?? false) &&
+      control.touched
     );
   }
 
