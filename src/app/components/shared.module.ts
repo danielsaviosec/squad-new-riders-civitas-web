ecbb597991c49785002d9766a22735e666a18e5e
Anna <acesbrama@gmail.com>
2024-11-04 12:55:36 -0300

    feat: icons do menu do sidebar adicionados

--- src/app/components/shared.module.ts
+++ src/app/components/shared.module.ts
@@ -7,14 +7,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     ButtonComponent,
     InputComponent,
     BackButtonComponent,
-    SidebarComponent
   ],
   imports: [
     CommonModule,
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { ILogo } from 'src/app/interface';
+import { ISidebarIcons } from 'src/app/interface/ISidebarIcons.interface';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,6 +9,7 @@ import { ILogo } from 'src/app/interface';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
+
   logoDesktop: ILogo[] = [
     { name: "Logo Civitas", image: 'assets/civitas-logos/logo_civitas_sem_fundo.webp' }
   ];
@@ -15,6 +17,8 @@ export class SidebarComponent {
   logoMobile: ILogo[] = [
     { name: "Civitas - logo em azul com escrita em preto", image: 'assets/civitas-logos/logo_completo_horizontal_civitas.webp' }
   ]
+
+  @Input() menuIcons: ISidebarIcons[] = [];
 
   constructor(private router: Router) { }
 
