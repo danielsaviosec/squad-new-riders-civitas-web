efeea95723adfccedf7ed0b0cb0a6bacb50fd1d2
Anna <acesbrama@gmail.com>
2024-11-04 13:51:20 -0300

    feat: criação do componente snackbar error

--- src/app/components/shared.module.ts
+++ src/app/components/shared.module.ts
@@ -7,12 +7,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { SnackbarErrorComponent } from './snackbar-error/snackbar-error.component';
 
 @NgModule({
   declarations: [
     ButtonComponent,
     InputComponent,
-    BackButtonComponent
+    BackButtonComponent,
+    SnackbarErrorComponent
   ],
   imports: [
     CommonModule,
