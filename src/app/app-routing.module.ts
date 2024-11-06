9d9ed1714ab7d27d20d947f64e5ebed8365b72cf
Anna <acesbrama@gmail.com>
2024-11-06 20:03:26 -0300

    feat: criação do student-registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminScreenComponent } from './pages/admin-screen/admin-screen.component';
 import { ClassRegistrationComponent } from './pages/class-registration/class-registration.component';
 import { AdminLoginComponent } from './pages/auth/admin-login/admin-login.component';
+import { StudentRegistrationComponent } from './pages/student-registration/student-registration.component';
 
 const routes: Routes = [
   { path: 'admin-screen', component: AdminScreenComponent },
   { path: 'class-registration', component: ClassRegistrationComponent },
+  { path: 'student-registration', component: StudentRegistrationComponent },
   { path: 'admin-login', component: AdminLoginComponent }
 ];
 
