7b26d61b84e9c156918ef77dbca7070ce3d741e5
Anna <acesbrama@gmail.com>
2024-11-07 16:06:59 -0300

    feat: rotas dos botões do sidebar e content

--- src/app/pages/teacher-screen/teacher-screen.component.ts
+++ src/app/pages/teacher-screen/teacher-screen.component.ts
@@ -10,7 +10,7 @@ import { ISidebarIcons } from 'src/app/interface';
 export class TeacherScreenComponent {
   icons: ISidebarIcons[] = [
     { name: "Início", image: 'assets/icons-sidebar/inicio.svg', route: '/' },
-    { name: "Turmas", image: 'assets/icons-sidebar/turmas.svg', route: '#' },
+    { name: "Turmas", image: 'assets/icons-sidebar/turmas.svg', route: 'main/listing-classes' },
   ];
 
   constructor(private router: Router) { }
