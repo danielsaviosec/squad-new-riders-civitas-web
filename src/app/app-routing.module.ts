e9907d3357c530f3958813403de360a1cd736b68
Anna Clara <148924947+annasbrama@users.noreply.github.com>
2024-11-07 20:15:10 -0300

    feat: criação do student-registration (#14)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminScreenComponent } from './pages/admin-screen/admin-screen.component';
+import { ClassRegistrationComponent } from './pages/class-registration/class-registration.component';
+import { AdminLoginComponent } from './pages/auth/admin-login/admin-login.component';
+import { StudentRegistrationComponent } from './pages/student-registration/student-registration.component';
 import { AuthGuard } from './pages/auth/auth.guard';
 
 const routes: Routes = [
+  { path: 'admin-screen', component: AdminScreenComponent },
+  { path: 'class-registration', component: ClassRegistrationComponent },
+  { path: 'student-registration', component: StudentRegistrationComponent },
+  { path: 'admin-login', component: AdminLoginComponent },
   {
     path: 'auth',
     loadChildren: () => import('./pages/auth/auth.module').then((m) => m.AuthModule),
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,28 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AdminScreenComponent } from './pages/admin-screen/admin-screen.component';
+import { AuthModule } from './pages/auth/auth.module';
+import { ClassRegistrationComponent } from './pages/class-registration/class-registration.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { SharedModule } from './components/shared.module';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { StudentRegistrationComponent } from './pages/student-registration/student-registration.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    AdminScreenComponent,
+    ClassRegistrationComponent,
+    SidebarComponent,
+    StudentRegistrationComponent
+  ],
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
