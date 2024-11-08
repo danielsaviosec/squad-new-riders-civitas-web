7d6dfc3881a26fc12ecd1db81064a55e015c0cf2
Anna Clara <148924947+annasbrama@users.noreply.github.com>
2024-11-08 07:47:28 -0300

    feat: criação da teacher-screen (#18)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AdminScreenComponent } from './pages/admin-screen/admin-screen.component';
+import { AuthModule } from './pages/auth/auth.module';
+import { ClassRegistrationComponent } from './pages/class-registration/class-registration.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { SharedModule } from './components/shared.module';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TeacherRegistrationComponent } from './pages/teacher-registration/teacher-registration.component';
+import { StudentRegistrationComponent } from './pages/student-registration/student-registration.component';
+import { TeacherScreenComponent } from './pages/teacher-screen/teacher-screen.component';
 
 @NgModule({
   declarations: [
@@ -12,8 +27,9 @@ import { AppComponent } from './app.component';
     AdminScreenComponent,
     ClassRegistrationComponent,
     SidebarComponent,
-    TeacherRegistrationComponent
-    StudentRegistrationComponent
+    TeacherRegistrationComponent,
+    StudentRegistrationComponent,
+    TeacherScreenComponent
   ],
   imports: [
     AppRoutingModule,
