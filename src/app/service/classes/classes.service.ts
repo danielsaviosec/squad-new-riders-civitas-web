d37d7c19bd1e8809efd055d0cbca1347aa284507
Vinícius <santsmarcos81@gmail.com>
2024-11-26 15:12:14 -0300

    Atualizado endpoint de atualizacao de turma

--- src/app/service/classes/classes.service.ts
+++ src/app/service/classes/classes.service.ts
@@ -42,6 +42,6 @@ export class ClassService {
     const token = localStorage.getItem('@civitas:token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-    return this.http.put<CreateResponse>(`${environment.apiUrl}admin/classes/${id}`, data, { headers });
+    return this.http.put<CreateResponse>(`${environment.apiUrl}classes/${id}`, data, { headers });
   }
 }
