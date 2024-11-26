16a071e9116e7be049a3b55986162bf581a3f5e9
Vinícius <santsmarcos81@gmail.com>
2024-11-26 18:04:04 -0300

    Integracao do formulario para cadastro ADI

---
+++ src/app/interface/register/IAdiRegistrationData.interface.ts
@@ -0,0 +1,9 @@
+export interface IAdiRegistrationData {
+  studentId: number;
+  selfAwareness: number;
+  empathy: number;
+  communication: number;
+  teamwork: number;
+  autonomy: number;
+  teacherComments: string;
+}
