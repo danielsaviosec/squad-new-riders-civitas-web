135db6d2aeb2a647a7ad43db869e92c3404cec67
Vinícius <santsmarcos81@gmail.com>
2024-11-27 08:48:52 -0300

    Configurando redirecionamento pos cadastro

--- src/app/pages/main/components/form-registration/form-registration.component.ts
+++ src/app/pages/main/components/form-registration/form-registration.component.ts
@@ -93,7 +93,7 @@ export class FormRegistrationComponent implements OnInit {
         })
       )
       .subscribe({
-        next: () => this.handleSuccess(),
+        next: (response) => this.handleSuccess(response?.data?.id),
         error: (data) => this.handleError(data?.error)
       })
     }
@@ -118,7 +118,7 @@ export class FormRegistrationComponent implements OnInit {
     return option ? option.grade : 0;
   }
 
-  private handleSuccess() {
+  private handleSuccess(id: number | undefined) {
     this._snackBar.open('ADI registrado com sucesso!', '', {
       duration: 3000,
       horizontalPosition: 'right',
@@ -126,7 +126,7 @@ export class FormRegistrationComponent implements OnInit {
     });
 
     setTimeout(() => {
-      this.router.navigate([`/main/adi-details}`])
+      this.router.navigate([`/main/adi-details/${id}}`])
     }, 3500);
   }
 
